07b1d7d7c98d28ffa4a5a91a238a3b893ec75952
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-11 00:01:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,18 @@
 sentence = "Hello there, and how are you?"
 
+# def reverse_each_word(sentence)
+#   new_sentence = []
+#   array = sentence.split
+#   array.each do |word|
+#     new_sentence << word.reverse
+#   end
+#   new_sentence.join(" ")
+# end 
+
 def reverse_each_word(sentence)
-  sentence.join
-  new_sentence = []
-  sentence.each do |word|
-    word.reverse
-    new_sentence << word
-  end
-  new_sentence.join
+  array = sentence.split
+  array.collect do |word|
+    new_sentence = word.reverse
+  end 
+  return new_sentence
 end
